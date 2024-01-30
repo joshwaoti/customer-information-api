26b42ef4a6086a36d5514a9a84ed548a75bc0fb9
joshwaoti <joshwaotieno643@gmail.com>
2024-01-30 22:06:15 +0300

    writing the model classes

--- base/models.py
+++ base/models.py
@@ -1,3 +1,40 @@
+from dateutil.relativedelta import relativedelta
 from django.db import models
+from django_countries.fields import CountryField
 
-# Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField( max_length=255)
+    customer_phone = models.CharField(max_length=20)
+    customer_email = models.EmailField()
+    date_of_birth = models.DateField()
+    nationality = CountryField()
+
+    def __str__(self):
+        return self.name
+    
+class Business_Category(models.Model):
+    title = models.CharField(max_length=255)
+
+
+class Location(models.Model):
+    county = models.CharField(max_length=255)
+    sub_county = models.CharField(max_length=255)
+    ward = models.CharField(max_length=255)
+    building_name = models.CharField(max_length=255, blank=True)
+    floor = models.IntegerField(blank=True)
+
+class Business(models.Model):
+    category = models.ForeignKey(Business_Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    registration_date = models.DateField()
+    age = models.PositiveIntegerField(editable=False)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.age = (models.DateField.today() - self.registration_date).days // 365.25
+        super().save(*args, **kwargs)
--- customer_business_management/settings.py
+++ customer_business_management/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third Party libraries
+    'rest_framework',
+    'django_countries ',
+
+    # Custom apps
+    'base',
 ]
 
 MIDDLEWARE = [
